6bbbec2d02161e646b118a44b022af5fb8fe0385
jcorpac <jcorpac@gmail.com>
2015-08-14 12:58:37 -0500

    Updated for new Project
    Change “Spotify Streamer” button to “Popular Movies”
    Comment out unneeded button object declarations.

--- app/src/main/java/com/jcorpac/udacity/myappportfolio/MainActivity.java
+++ app/src/main/java/com/jcorpac/udacity/myappportfolio/MainActivity.java
@@ -3,7 +3,6 @@ package com.jcorpac.udacity.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,39 +12,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSpotify = (Button) findViewById(R.id.btnSpotify);
+        /*
+        Button btnMovies = (Button) findViewById(R.id.btnMovies);
         Button btnScores = (Button) findViewById(R.id.btnScores);
         Button btnLibrary = (Button) findViewById(R.id.btnLibrary);
         Button btnBuildItBigger = (Button) findViewById(R.id.btnBuildItBigger);
         Button btnXYZReader = (Button) findViewById(R.id.btnXYZReader);
         Button btnCapstone = (Button) findViewById(R.id.btnCapstone);
+        */
     }
 
-    public void onClickedSpotify(View view) {
-        makeToast("Let's play some music! - Coming soon");
+    public void onClickedMovies(View view) {
+        makeToast("What movies are playing? - Coming soon", false);
     }
 
     public void onClickedScores(View view) {
-        makeToast("Yay, Sportsball! - Coming soon");
+        makeToast("Yay, Sportsball! - Coming soon", false);
     }
 
     public void onClickedLibrary(View view) {
-        makeToast("Shh! It's a Library! - Coming soon");
+        makeToast("Shh! It's a Library! - Coming soon", false);
     }
 
     public void onClickedBuildItBigger(View view) {
-        makeToast("Bigger and Better! - Coming soon");
+        makeToast("Bigger and Better! - Coming soon", false);
     }
 
     public void onClickedReader(View view) {
-        makeToast("XYZ, easy as ABC? - Coming soon");
+        makeToast("XYZ, easy as ABC? - Coming soon", false);
     }
 
     public void onClickedCapstone(View view) {
-        makeToast("I'm wondering what it'll be too. - Coming soon");
+        makeToast("I'm wondering what it'll be too. - Coming soon", false);
     }
 
-    private void makeToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    private void makeToast(String message, boolean isLong) {
+        Toast.makeText(getApplicationContext(), message, isLong? Toast.LENGTH_LONG:Toast.LENGTH_SHORT).show();
     }
 }
